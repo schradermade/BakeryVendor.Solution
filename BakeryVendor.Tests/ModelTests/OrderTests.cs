e07240427e3f79438c1b2434d426abc0a9b86080
Nathan Schrader <nathanschrader@icloud.com>
2020-12-18 10:50:34 -0800

    add good fail: GetOrder tests

---
+++ BakeryVendor.Tests/ModelTests/OrderTests.cs
@@ -0,0 +1,23 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Bakery;
+using System;
+
+namespace Bakery.Test
+{
+  [TestClass]
+  public class OrderTests
+  {
+    // public void Dispose()
+    // {
+    //   OrderTests.ClearAll();
+    // }
+
+    [TestMethod]
+    public void GetOrder_CreatesInstanceOfItem_Order()
+    {
+      Order newOrder = new Order();
+      Assert.AreEqual(typeof(Bread), newOrder.GetType());
+    }
+  }
+}
