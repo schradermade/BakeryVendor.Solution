399f264c598ef405a7e9795bed812200935cc5c8
Nathan Schrader <nathanschrader@icloud.com>
2020-12-20 15:10:10 -0800

    add Good Fail : VendorConstructor test

---
+++ BakeryVendor.Tests/ModelTests/VendorTests.cs
@@ -0,0 +1,30 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Bakery.Models;
+using System;
+
+namespace Bakery.Test
+{
+  [TestClass]
+  public class VendorTests : IDisposable
+  {
+    public void Dispose()
+    {
+      Vendor.ClearAll();
+    }
+
+    [TestMethod]
+    public void VendorConstructor_CreatesInstanceOfVendor_True()
+    {
+      // Arrange
+      string name = "Starbucks";
+      string description = "coffee shop";
+      Vendor newVendor = new Vendor(name, description);
+
+      // Assert
+      Assert.AreEqual(typeof(Order), newVendor.GetType());
+      
+
+    }
+  }
+}
