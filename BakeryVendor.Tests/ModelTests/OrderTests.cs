7f74cc17aa0d7a08b1a24ecbc5c619c2e926dc50
Nathan Schrader <nathanschrader@icloud.com>
2020-12-20 13:08:31 -0800

    This commit represents a clean start from previously having messed up my git history. I believe I accidently 'rebased' my git and I didnt know how to work my way out of it.

--- BakeryVendor.Tests/ModelTests/OrderTests.cs
+++ BakeryVendor.Tests/ModelTests/OrderTests.cs
@@ -16,42 +16,46 @@ namespace Bakery.Test
     [TestMethod]
     public void OrderConstructor_CreatesInstanceOfOrder_True()
     {
-      int newId = 5;
-      DateTime date = Order.TimeDateStamp();
-      Order newOrder = new Order(date, newId);
+      string title = "Starbucks";
+      double price = 10;
+      // DateTime date = Order.TimeDateStamp();
+      Order newOrder = new Order(title, price);
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
 
     [TestMethod]
     public void AssignId_CreatesInstanceOfId_Id()
     {
-      int newId = 5;
-      DateTime date = Order.TimeDateStamp();
-      Order newOrder = new Order(date, newId);
+      string title = "Starbucks";
+      double price = 10;
+      // DateTime date = Order.TimeDateStamp();
+      Order newOrder = new Order(title, price);
       Assert.AreEqual(typeof(Order), newOrder.GetType());
     }
 
     [TestMethod]
     public void GetId_CreatesUniqueIdForOrder_Int()
     { // Arrange
-      int newId = 1;
-      DateTime date = Order.TimeDateStamp();
-      Order newOrder = new Order(date, newId);
+      string title = "Starbucks";
+      double price = 10;
+      // DateTime date = Order.TimeDateStamp();
+      Order newOrder = new Order(title, price);
       
       // Act
       int result = newOrder.Id;
 
       // Assert
-      Assert.AreEqual(newId, result);
+      Assert.AreEqual(newOrder.Id, result);
     }
 
     [TestMethod]
     public void GetAll_ReturnsEmptyList_True()
     {
       // Arrange
-      int newId = 5;
-      DateTime date = Order.TimeDateStamp();
-      Order newOrder = new Order(date, newId);
+      string title = "Starbucks";
+      double price = 10;
+      // DateTime date = Order.TimeDateStamp();
+      Order newOrder = new Order(title, price);
       List<Order> newList = new List<Order> { };
       newList.Add(newOrder);
       Console.WriteLine(newList);
@@ -68,9 +72,11 @@ namespace Bakery.Test
     public void ClearAll_ClearsAllItems_True()
     {
       // Arrange
-      DateTime date = Order.TimeDateStamp();
-      Order newOrder0 = new Order(date, 5);
-      Order newOrder1 = new Order(date, 5);
+      string title = "Starbucks";
+      double price = 10;
+      // DateTime date = Order.TimeDateStamp();
+      Order newOrder0 = new Order(title, price);
+      Order newOrder1 = new Order(title, price);
       List<Order> newList = new List<Order> { };
 
       // Act
@@ -81,31 +87,46 @@ namespace Bakery.Test
       CollectionAssert.AreEqual(newList, result);
     }
 
-    [TestMethod]
-    public void GetDate_ReturnTimeDateProperty_True()
-    {
-      // Arrange
-      DateTime date = Order.TimeDateStamp();
-      int newId = 5;
-      Order newOrder = new Order(date, newId);
-      // Act
-      DateTime result = Order.TimeDateStamp();
-      // Assert
-      Assert.AreEqual(date, result);
-    }
+    // [TestMethod]
+    // public void TimeDateStamp_ReturnTimeDateProperty_True()
+    // {
+    //   // Arrange
+    //   // DateTime date = Order.TimeDateStamp();
+    //   int newId = 5;
+    //   string title = "Starbucks";
+    //   Order newOrder = new Order(title, newId);
+    //   // Act
+    //   ;
+    //   // Assert
+    //   Assert.AreEqual(title, result);
+    // }
 
     [TestMethod]
     public void SetPrice_ReturnsSetPrice_Price()
     {
       // Arrange
-      int newId = 5;
-      DateTime date = Order.TimeDateStamp();
-      double price = 5;
-      Order newOrder = new Order(date, newId);
+      string title = "Starbucks";
+      double price = 10;
+      // DateTime date = Order.TimeDateStamp();
+      Order newOrder = new Order(title, price);
       // Act
       double result = newOrder.Price;
       // Assert
-      Assert.AreEqual(price, result);
+      Assert.AreEqual(result, newOrder.Price);
+    }
+
+    [TestMethod]
+    public void GetPrice_ReturnsPrice_Price()
+    {
+      // Arrange
+      string title = "Starbucks";
+      double price = 10;
+      // DateTime date = Order.TimeDateStamp();
+      Order newOrder = new Order(title, price);
+      // Act
+      double result = newOrder.Price;
+      // Assert
+      Assert.AreEqual(result, newOrder.Price);
     }
 
   }
