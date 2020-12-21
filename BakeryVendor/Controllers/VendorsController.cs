18ae1010760a7489260595120a48503c4acd232e
Nathan Schrader <nathanschrader@icloud.com>
2020-12-20 23:42:00 -0800

    add Post and Get requests

---
+++ BakeryVendor/Controllers/VendorsController.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.Mvc;
+using Bakery.Models;
+using System.Collections.Generic;
+using System;
+
+namespace Bakery.Controllers
+{
+  public class VendorsController : Controller
+  {
+    [HttpGet("/vendors")]
+    public ActionResult Index()
+    {
+      List<Vendor> allVendors = Vendor.GetAll();
+      return View(allVendors);
+    }
+
+    [HttpGet("/vendor/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpPost("/Vendors")]
+    public ActionResult Create(string name, string description)
+    {
+      Vendor newVendor = new Vendor(name, description);
+      return RedirectToAction("Index");
+    }
+
+    [HttpPost("/vendors/delete")]
+    public ActionResult DeleteAll()
+    {
+      Vendor.ClearAll();
+      return RedirectToAction("Index");
+    }
+
+    [HttpGet("/vendors/{id}")]
+    public ActionResult Show(int id)
+    {
+      Vendor foundVendor = Vendor.Find(id);
+      return View(foundVendor);
+    }
+
+    [HttpPost("/vendors/{id}")]
+    public ActionResult Destroy(int id)
+    {
+      Vendor foundVendor = Vendor.Find(id);
+      Vendor.DeleteVendor(foundVendor.Id);
+      return RedirectToAction("Index");
+    }
+  }
+}
