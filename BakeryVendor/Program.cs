f84652e0156e1551aee663deb00478b8b8949593
Nathan Schrader <nathanschrader@icloud.com>
2020-12-20 17:38:02 -0800

    add WebHostBuilder

--- BakeryVendor/Program.cs
+++ BakeryVendor/Program.cs
@@ -5,9 +5,16 @@ namespace Bakery
 {
   public class Programs
   {
-    static void Main()
+    static void Main(string[] args)
     {
-      
+      var host = new WebHostBuilder()
+        .UseKestrel()
+        .UseContentRoot(Directory.GetCurrentDirectory())
+        .UseIISIntegration()
+        .UseStartup<Startup>()
+        .Build();
+
+      host.Run();
     }
   }
 }
